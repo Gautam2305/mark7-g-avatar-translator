dc6fbc84749c3147a292f877f9538e46eb587481
harshitadatra <95231941+Gautam2305@users.noreply.github.com>
2021-12-05 20:49:17 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("this is working from a different file");
 var username=prompt("Give me your name");
-alert("the name is "+ username);
+ var welcomeMsg = "this script works "+ username;
+ alert(welcomeMsg);
