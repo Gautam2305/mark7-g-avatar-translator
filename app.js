09795501cededd667ef3ed963a5d055a62b80396
harshitadatra <95231941+Gautam2305@users.noreply.github.com>
2021-12-05 22:52:48 +0530

    wire fetch call

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
 var outputDiv= document.querySelector("#output");
+var url = "https://api.funtranslations.com/translate/navi.json";
 
 
-console.log(txtInput);
-function clickEventHandler(){
-    console.log("clicked!");
-    console.log("input" , txtInput.value);
-    outputDiv.innertext =" "+ txtInput.value
+function getTranlasationURL(text){
+    return url +"?"+"text="+text
+
 }
-
-
-
-
-
-
+function clickEventHandler(){
+    var innerText = txtInput.value ;
+    fetch(getTranlasationURL(innerText))
+    .then(response=>response.json())
+    .then(json => { 
+        var translatedText = json.contents.translated;
+    outputDiv.innerText= translatedText}
+       )
+    
+    
+}
 
 btnTranslate.addEventListener("click",clickEventHandler)
